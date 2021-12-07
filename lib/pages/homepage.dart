0803e66a8695f97515eb747d013e381bf29018ae
Dark angel <50552311+darkamgel@users.noreply.github.com>
2021-12-07 21:12:55 +0545

    mapping json data

--- lib/pages/homepage.dart
+++ lib/pages/homepage.dart
@@ -1,17 +1,48 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_catalog/models/catalog.dart';
 import 'package:flutter_catalog/widgets/drawer.dart';
 import 'package:flutter_catalog/widgets/item_widgets.dart';
+import 'dart:convert';
 
-class Homepage extends StatelessWidget {
+class Homepage extends StatefulWidget {
   const Homepage({Key? key}) : super(key: key);
 
+  @override
+  State<Homepage> createState() => _HomepageState();
+}
+
+class _HomepageState extends State<Homepage> {
   final int days = 30;
+
   final String name = "Sandeep";
 
   @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    loadData();
+  }
+
+  loadData() async {
+    // await Future.delayed(Duration(seconds: 10));
+    final catalogJson =
+        await rootBundle.loadString("assets/files/catalog.json");
+    // print(catalogjson);
+    final decodedData = jsonDecode(catalogJson);
+    // print(decodedData);
+    var productsData = decodedData["products"];
+    // print(productsData);
+    CatalogeModel.items = List.from(productsData)
+        .map<Item>((item) => Item.fromMap(item))
+        .toList();
+
+    setState(() {});
+  }
+
+  @override
   Widget build(BuildContext context) {
-    final dummyList = List.generate(4, (index) => CatalogeModel.items[0]);
+    // final dummyList = List.generate(4, (index) => CatalogeModel.items[0]);
 
     return Scaffold(
       appBar: AppBar(
@@ -41,15 +72,15 @@ class Homepage extends StatelessWidget {
       // ),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
-        child: ListView.builder(
+        child: (CatalogeModel.items != null && CatalogeModel.items.isNotEmpty) ? ListView.builder(
             // itemCount: CatalogeModel.items.length,
-            itemCount: dummyList.length,
+            itemCount: CatalogeModel.items.length,
             itemBuilder: (context, index) {
               return ItemWidget(
                 // item: CatalogeModel.items[index],
-                item: dummyList[index],
+                item: CatalogeModel.items[index],
               );
-            }),
+            }): Center(child: CircularProgressIndicator(),),
       ),
 
       drawer: MyDrawer(),
